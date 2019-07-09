da27d8bce5806e95729d5d3102c0f37b4bf9da6c
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2019-07-09 15:58:01 -0400

    add boilerplate

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// install -> import -> use
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/style.scss';
+import 'normalize.css/normalize.css';
+
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'))
+
